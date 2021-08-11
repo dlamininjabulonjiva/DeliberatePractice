170e9f7fb76afc83fc6a5d796fc8df8c7c3e2ac7
Njabulo Dlamini <njabulo.dlamini@chillisoft.co.za>
2021-08-11 19:13:50 +0200

    my first time ever working with feature toggles

---
+++ FeatureToggles/MyFeatureToggles/MyFeatureToggles/Controllers/SportController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Microsoft.FeatureManagement;
+using Microsoft.FeatureManagement.Mvc;
+
+namespace MyFeatureToggles.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class SportController : Controller
+  {
+    private readonly IFeatureManager _featureManager;
+
+    public SportController(IFeatureManager featureManager)
+    {
+      _featureManager = featureManager;
+    }
+
+    [HttpGet("news")]
+    [FeatureGate("SportNewsEnabled")]
+    public string GetSportNews()
+    {
+      const string news = "The Springboks have beaten the British & Irish Lions " +
+                          "by 19 points to 16. It was a tough match but guys like " +
+                          "Franco Mostert, Lukhanyo AM, Cheslin Kolbe and Lood de Jager were at the top of their game";
+      return news;
+    }
+
+    [HttpGet("fixture")]
+    public async Task<string> GetFixture()
+    {
+      var isGameOnASaturday = await _featureManager.IsEnabledAsync("SaturdayGameEnabled");
+      var matchDay = isGameOnASaturday ? "Game will be played on a Saturday" : "Game will be played on a Sunday";
+      return matchDay;
+    }
+  }
+}
---
+++ FeatureToggles/MyFeatureToggles/MyFeatureToggles/Startup.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.FeatureManagement;
+
+namespace MyFeatureToggles
+{
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddFeatureManagement();
+      services
+        .AddControllers()
+        .AddJsonOptions(option =>
+        {
+          option.JsonSerializerOptions.IgnoreNullValues = true;
+        });
+      services.AddControllers();
+      services.AddSwaggerGen();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      app.UseSwagger();
+      app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "feature_toggles v1"));
+
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
+}
