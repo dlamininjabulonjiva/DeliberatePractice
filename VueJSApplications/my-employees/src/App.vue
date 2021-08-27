9afa74a9f71fde045e7b110b7cef3c4f08727e7b
Njabulo Dlamini <njabulo.dlamini@chillisoft.co.za>
2021-08-27 16:30:30 +0200

    small change

--- VueJSApplications/my-employees/src/App.vue
+++ VueJSApplications/my-employees/src/App.vue
@@ -39,14 +39,14 @@
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin-top: 60px;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    margin-top: 60px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+  }
 </style>
