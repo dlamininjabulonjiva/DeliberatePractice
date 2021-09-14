a45830fbe1c6f7b0b940beb0684d6c727c5d91b8
Njabulo Dlamini <njabulo.dlamini@chillisoft.co.za>
2021-09-14 12:22:06 +0200

    added tests

---
+++ VueJSApplications/company-employees/tests/unit/components/tabNav.spec.js
@@ -0,0 +1,66 @@
+import { shallowMount } from '@vue/test-utils'
+import TabNav from '@/components/TabNav.vue'
+
+describe('TabNav.vue', () => {
+  describe('Props', () => {
+    it('should have props', () => {
+      // Arrange
+      const tabs = [];
+      const selected = '';
+      const wrapper = wrapperFactory(tabs, selected);
+      // Act
+      const actual = wrapper.props();
+      // Assert
+      expect(actual.tabs).toBeDefined();
+      expect(actual.selected).toBeDefined();
+    });
+
+    it('should set values to props', () => {
+      // Arrange
+      const tabs = ['tab1', 'tab2', 'tab3'];
+      const selected = 'tab1';
+      const wrapper = wrapperFactory(tabs, selected);
+      // Act
+      const actual = wrapper.props();
+      // Assert
+      expect(actual.tabs).toStrictEqual(tabs);
+      expect(actual.selected).toBe(selected);
+    });
+  });
+
+  describe('Methods', () => {
+    it("should call setTab method when a tab is clicked", () => {
+      // Arrange
+      const tabs = ['tab1', 'tab2', 'tab3'];
+      const selected = 'tab1';
+      const wrapper = wrapperFactory(tabs, selected);
+      spyOn(wrapper.vm, "setTab");
+      // Act
+      wrapper.find(".nav-link").trigger("click");
+      // Assert
+      expect(wrapper.vm.setTab).toBeCalledTimes(1);
+    });
+  
+    it("should trigger selected event when the setTab method is called", () => {
+      // Arrange
+      const tabs = ['tab1', 'tab2', 'tab3'];
+      const selected = 'tab1';
+      const wrapper = wrapperFactory(tabs, selected);
+      const stub = jest.fn();
+      wrapper.vm.$on("selected", stub);
+      // Act
+      wrapper.vm.setTab(selected);
+      // Assert
+      expect(stub).toBeCalledWith(selected);
+    });
+  });
+
+  function wrapperFactory(tabs, selected) {
+    return shallowMount(TabNav, {
+      propsData: {
+        tabs: tabs,
+        selected: selected
+      }
+    })
+  }
+})
