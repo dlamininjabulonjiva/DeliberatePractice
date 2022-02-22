f6bf148512723c441bbf7c8f9090d31f4183443b
Njabulo Dlamini <njabulo.dlamini@chillisoft.co.za>
2022-02-22 15:58:24 +0200

    product details screen design

--- ReactApplications/my-order-system/src/Components/Product/ProductDetails.tsx
+++ ReactApplications/my-order-system/src/Components/Product/ProductDetails.tsx
@@ -1,6 +1,8 @@
 import { LinearProgress } from "@material-ui/core";
 import { useParams } from "react-router-dom";
 import { GetProductById } from '../../Services/MyApiService';
+import Button from '@material-ui/core/Button';
+import FormatCurrency  from '../../Helpers/FormatCurrency';
 import './ProductDetails.scss';
 
 type ProductDetailsProps = {
@@ -9,7 +11,9 @@ type ProductDetailsProps = {
 const ProductDetails: React.FC<ProductDetailsProps> = () => {
     const { productId } = useParams();
     const id: string = productId ?? '';
+    
     const product = GetProductById(id);
+    const price: number = product.data?.price ?? 0;
 
     if (!product.doneLoading) {
         return <LinearProgress />;
@@ -21,7 +25,24 @@ const ProductDetails: React.FC<ProductDetailsProps> = () => {
      
     return (
         <div className="ProductDetails">
-            <h2>My Details : {product.data?.category}</h2>
+            <div className="left-column">
+                <img src={product.data?.image} alt={product.data?.title} />
+            </div>
+            <div className="right-column">
+                <div className="product-description">
+                    <span>{product.data?.category}</span>
+                    <h2>{product.data?.title}</h2>
+                    <p>{product.data?.description}</p>
+                </div>
+                <div className="reviews">
+                    <span>Reviews</span>
+                    <p>{product.data?.rating.rate} Rating with {product.data?.rating.count} Reviews.</p>
+                </div>
+                <div className="product-price">
+                    <span className="price">{FormatCurrency(price)}</span>
+                    <Button variant="outlined" color="primary">Add to cart</Button>
+                </div>
+            </div>
         </div>
     );
 };
