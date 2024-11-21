8a62e30eacf792f33191e8da835b5ded4a326f5e
njabulo-dlamini <njabulo.dlamini@chillisoft.co.za>
2024-11-21 16:00:55 +0200

    deleted

--- Amanzimtoti Rugby Club/BackEnd/amanzimtoti-rugby-club/src/pages/dashboard/Players.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Typography } from '@mui/material'
-
-const Players: React.FC = () => {
-  return <Typography variant="h2" color="primary">Player Management</Typography>
-}
-
-export default Players
--- Amanzimtoti Rugby Club/BackEnd/amanzimtoti-rugby-club/src/pages/dashboard/TeamSetup.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Typography } from '@mui/material'
-
-const TeamSetup: React.FC = () => {
-  return <Typography variant="h2" color="primary">Team Setup</Typography>
-}
-
-export default TeamSetup
