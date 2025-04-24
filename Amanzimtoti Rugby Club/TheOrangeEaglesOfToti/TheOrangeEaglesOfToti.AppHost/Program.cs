6ebdeff9a50a0c7c2286490b19402ae330f5d241
njabulo-dlamini <njabulo.dlamini@chillisoft.co.za>
2025-04-24 15:20:21 +0200

    add db

--- Amanzimtoti Rugby Club/TheOrangeEaglesOfToti/TheOrangeEaglesOfToti.AppHost/Program.cs
+++ Amanzimtoti Rugby Club/TheOrangeEaglesOfToti/TheOrangeEaglesOfToti.AppHost/Program.cs
@@ -4,6 +4,12 @@ var cache = builder.AddRedis("cache");
 
 var apiService = builder.AddProject<Projects.TheOrangeEaglesOfToti_ApiService>("apiservice");
 
+var username = builder.AddParameter("user", secret: true, value: "postgres");
+var password = builder.AddParameter("password", secret: true, value: "postgres");
+
+var postgresServer = builder.AddPostgres("postgres", username, password);
+var progressionFrameworkDb = postgresServer.AddDatabase("TheOrangeEagleDB");
+
 builder.AddProject<Projects.TheOrangeEaglesOfToti_Web>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithReference(cache)
--- Amanzimtoti Rugby Club/TheOrangeEaglesOfToti/TheOrangeEaglesOfToti.Tests/obj/Debug/net8.0/TheOrangeEaglesOfToti.Tests.AssemblyInfo.cs
+++ Amanzimtoti Rugby Club/TheOrangeEaglesOfToti/TheOrangeEaglesOfToti.Tests/obj/Debug/net8.0/TheOrangeEaglesOfToti.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TheOrangeEaglesOfToti.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa4d1a20bd3aaf2db6389145379880902ae054ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+728785195187101a43b6917da8b7e2c2ab838b53")]
 [assembly: System.Reflection.AssemblyProductAttribute("TheOrangeEaglesOfToti.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TheOrangeEaglesOfToti.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
